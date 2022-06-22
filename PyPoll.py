c99e84cf3df711664e0b2bdf16a60f1c8662179a
Keerti Chaudhary <17keerti@gmail.com>
2022-06-22 15:33:34 -0700

    get total number of votes and candidate list

--- PyPoll.py
+++ PyPoll.py
@@ -4,3 +4,43 @@
 # 3. Total number of votes each candidate received
 # 4. Percentage of votes each candidate won
 # 5. The winner of the election based on popular vote
+
+import csv
+import os
+# Assign a variable for the file to load and the path.
+file_to_load = os.path.join("resources", "election_results.csv")
+
+# Assign a variable to save the file to a path.
+file_to_save = os.path.join("analysis", "election_analysis.txt")
+
+# Initialize a total vote counter.
+total_votes = 0
+# Initialize a candidate options list
+candidate_options = []
+
+# Open the election results and read the file.
+with open(file_to_load) as election_data:
+
+  # Read the file object with the reader function.
+    file_reader = csv.reader(election_data)
+      # Read the header row.
+    headers = next(file_reader)
+
+    # Print each row in the CSV file.
+    for row in file_reader:
+        # Add to the total vote count.
+        total_votes += 1
+
+        # Print the candidate name from each row.
+        candidate_name = row[2]
+
+        # If the candidate does not match any existing candidate...
+        if candidate_name not in candidate_options:
+            # Add it to the list of candidates.
+            candidate_options.append(candidate_name)
+
+# Print the total votes.
+print(total_votes)
+
+# Print Candidate list
+print(candidate_options)
